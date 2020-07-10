3b70e325166992e135194587c718f8ccea0f82c6
Asfand Qazi <asfand@cloudership.com>
2020-07-10 23:13:12 +0100

    Generate presigned URLs to allow uploads to (as yet nonexistent) bucket

--- upload_link_generator.py
+++ upload_link_generator.py
@@ -1,15 +1,49 @@
 import json
+import os
+import uuid
+
+import boto3
+from botocore.client import Config
 
 
-def handler(event, context):
-    body = {
-        "message": "upload_link_generator.handler",
-        "input": event
-    }
+def handler(event, _context):
+    if os.environ.get('DEBUG') == 'true':
+        print(json.dumps(event))
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    media_id = str(uuid.uuid4())
+    bucket = os.environ['UPLOAD_BUCKET']
+    filename = 'upload/' + media_id
+    status_code = 200
+    body = {}
 
-    return response
+    try:
+        s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
+        post = s3.generate_presigned_post(
+            Bucket=bucket,
+            Key=filename,
+            ExpiresIn=600,
+            Fields={
+                'x-amz-meta-id': media_id,
+            },
+            Conditions=[
+                {'x-amz-meta-id': media_id},
+            ]
+        )
+
+        body = {
+            'post': post,
+            'media_id': media_id,
+            'filename': filename
+        }
+
+    except Exception as e:
+        print('Exception: %s' % e)
+        status_code = 500
+        raise
+
+    finally:
+        return {
+            'statusCode': status_code,
+            'body': json.dumps(body),
+            'headers': {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
+        }
