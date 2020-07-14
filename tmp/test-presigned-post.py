d9efee87a5517020015d7927d0e0d081be5f3e16
Asfand Qazi <asfand@cloudership.com>
2020-07-14 03:01:03 +0100

    OK we know uploading works

--- tmp/test-presigned-post.py
+++ tmp/test-presigned-post.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import os
+import sys
 
 import boto3
 from botocore.config import Config
 import requests
 
 media_id = "deadbeef"
-key = "test-upload.mp4"
+key = "test-upload.data"
 bucket = "com-cloudership-eu-west-1-frootube-video-upload"
 # bucket = "a1b1-playground-scratch"
 
@@ -26,9 +27,14 @@ response = s3.generate_presigned_post(
 print("presigned post response")
 print(response)
 
-file = "/home/ayqazi/test-video.mp4"
-with open(file, "rb") as f:
-    files = {"file": (key, f)}
-    http_response = requests.post(response['url'], data=response['fields'], files=files)
+# file = "test/testdata/test-video.mp4"
+file = sys.argv[1]
 
-print(http_response)
+curl_fields = ' '.join([f"-F '{k}={v}'" for k, v in response['fields'].items()])
+print(f"curl -vv {curl_fields} -F 'file=@{file}' -L {response['url']}")
+
+# with open(file, "rb") as f:
+#     files = {"file": (key, f)}
+#     http_response = requests.post(response['url'], data=response['fields'], files=files)
+#
+# print(http_response)
